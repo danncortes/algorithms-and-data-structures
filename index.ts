9d64c244f3f87f58c876c9b8bbb913c49342f84f
Daniel Cortes <danncortes@gmail.com>
2025-08-28 12:54:57 +0200

    feat(binary-search-tree): add find method and improve insert method
    - Add find method to search for values in the BST
    - Modify insert method to return the tree instance for chaining
    - Add example usage in exported function

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ import radixSort from './src/sorting-algorithms/radix-sort';
 import singlyLinkedList from './src/data-structures/singly-linked-list';
 import doubleLinkedList from './src/data-structures/double-linked-list';
 import stacks from './src/data-structures/stacks';
+import binarySearchTree from './src/data-structures/binary-search-tree';
 
 // same();
 // findIndex();
@@ -30,5 +31,6 @@ import stacks from './src/data-structures/stacks';
 // quickSort();
 // radixSort();
 // singlyLinkedList()
-//doubleLinkedList();
-stacks()
+// doubleLinkedList();
+// stacks();
+binarySearchTree();
--- src/data-structures/binary-search-tree.ts
+++ src/data-structures/binary-search-tree.ts
@@ -1,11 +1,12 @@
 class BinarySearchTree {
-  root: TreeNode | null = null;
+    root: TreeNode | null = null;
 
-  insert(value: number) {
+    insert(value: number) {
     const node = new TreeNode(value);
 
     if(!this.root) {
         this.root = node;
+        return this;
     } else {
         let current = this.root;
 
@@ -13,21 +14,37 @@ class BinarySearchTree {
             if(value < current.value) {
                 if(!current.left) {
                     current.left = node;
-                    break;
+                    return this;
                 } else {
                     current = current.left;
                 }
             } else {
                 if(!current.right) {
                     current.right = node;
-                    break;
+                    return this;
                 } else {
                     current = current.right;
                 }
             }
         }
     }
-  }
+    }
+
+    find(value: number) {
+        let current: TreeNode | null = this.root;
+        while(true) {
+            if(!current) {
+                return null;
+            }
+            if(value === current.value) {
+                return current.value;
+            } else if(value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+    }
 }
 
 class TreeNode {
@@ -39,5 +56,17 @@ class TreeNode {
         this.left = null;
         this.right = null;
     }
+}
+
+export default function binarySearchTree() {
+    const bst =  new BinarySearchTree();
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(15);
+    bst.insert(2);
+    bst.insert(7);
+    bst.insert(12);
+    bst.insert(17);
+    console.log(bst.find(22));
 }
 
