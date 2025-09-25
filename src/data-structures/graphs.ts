65d014665d40561ac5387eea265f443210321889
Daniel Cortes <danncortes@gmail.com>
2025-09-25 13:11:41 +0200

    feat(graph): implement depth-first search algorithm
    Add depthFirstSearch method to Graph class to traverse graph nodes in depth-first order. Also update test case to demonstrate the new functionality.

--- src/data-structures/graphs.ts
+++ src/data-structures/graphs.ts
@@ -1,49 +1,101 @@
 class Graph {
     adjacencyList: { [key: string]: string[]} = {}
-  constructor() {
-  }
-  addVertex(vertex: string) {
-    if(!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
+    constructor() {
     }
-  }
-
-  removeVertex(vertex: string) {
-    if(vertex in this.adjacencyList) {
-        const edges = this.adjacencyList[vertex];
-        for(let edge of edges) {
-            this.removeEdge(vertex, edge);
+    addVertex(vertex: string) {
+        if(!this.adjacencyList[vertex]) {
+        this.adjacencyList[vertex] = [];
         }
-        delete this.adjacencyList[vertex];
     }
-  }
 
-  addEdge(vertex1: string, vertex2: string) {
-    if(vertex1 in this.adjacencyList && vertex2 in this.adjacencyList) {
-      this.adjacencyList[vertex1].push(vertex2);
-      this.adjacencyList[vertex2].push(vertex1);
+    removeVertex(vertex: string) {
+        if(vertex in this.adjacencyList) {
+            const edges = this.adjacencyList[vertex];
+            for(let edge of edges) {
+                this.removeEdge(vertex, edge);
+            }
+            delete this.adjacencyList[vertex];
+        }
     }
-  }
 
-  removeEdge(vertex1: string, vertex2: string) {
-    if(vertex1 in this.adjacencyList && vertex2 in this.adjacencyList) {
-      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
-      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    addEdge(vertex1: string, vertex2: string) {
+        if(vertex1 in this.adjacencyList && vertex2 in this.adjacencyList) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+        }
     }
-  }
+
+    removeEdge(vertex1: string, vertex2: string) {
+        if(vertex1 in this.adjacencyList && vertex2 in this.adjacencyList) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+        }
+    }
+
+    depthFirstSearch(start: string): string[] {
+        const visited: { [key: string]: boolean } = {};
+        
+        function visitVertex(list: string[], visited: { [key: string]: boolean }, adjacencyList: { [key: string]: string[] }): string[] {
+            const next = list.filter((v: string) => !visited[v]);
+
+            for(let i = 0; i < next.length; i++) {
+                const nextVertex = next[i];
+                visited[nextVertex] = true;
+                if(adjacencyList[nextVertex].some((v: string) => !visited[v])) {
+                    return [nextVertex, ...visitVertex(adjacencyList[nextVertex], visited, adjacencyList)]
+                } else {
+                    const newList = next.slice(i + 1);
+                    if(newList.length) {
+                        return [nextVertex, ...visitVertex(newList, visited, adjacencyList)]
+                    } else {
+                        return [nextVertex];
+                    }
+                }
+            }
+            
+            return [];
+        }
+
+        visited[start] = true;
+        
+        return [start, ...visitVertex(this.adjacencyList[start], visited, this.adjacencyList)];
+    }
 }
 
 export default () => {
     const graph = new Graph();
-    graph.addVertex('A');
-    graph.addVertex('B');
-    graph.addVertex('C');
-    graph.addVertex('D');
-    graph.addVertex('E');
-    graph.addEdge('A', 'B');
-    graph.addEdge('A', 'C');
-    graph.addEdge('B', 'D');
-    graph.addEdge('C', 'E');
-    graph.addEdge('D', 'E');
-    console.log("🚀 ~ graph:", graph);
+    graph.addVertex('S');
+graph.addVertex('P');
+graph.addVertex('U');
+graph.addVertex('X');
+graph.addVertex('Q');
+graph.addVertex('Y');
+graph.addVertex('V');
+graph.addVertex('R');
+graph.addVertex('W');
+graph.addVertex('T');
+ 
+graph.addEdge('S','P');
+graph.addEdge('S','U');
+ 
+graph.addEdge('P','X');
+graph.addEdge('U','X');
+ 
+graph.addEdge('P','Q');
+graph.addEdge('U','V');
+ 
+graph.addEdge('X','Q');
+graph.addEdge('X','Y');
+graph.addEdge('X','V');
+ 
+graph.addEdge('Q','R');
+graph.addEdge('Y','R');
+ 
+graph.addEdge('Y','W');
+graph.addEdge('V','W');
+ 
+graph.addEdge('R','T');
+graph.addEdge('W','T');
+    console.log(graph.adjacencyList);
+    console.log(graph.depthFirstSearch('S'));
 }
